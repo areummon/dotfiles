2031a348d083124ca46322748799bbff687cd84b
moka <areumm@proton.me>
2026-08-28 16:40:46 -0600

    gtk theme now dark and xdg portals problem solved

--- desktop/apps/agents/opencode.nix
+++ desktop/apps/agents/opencode.nix
@@ -8,10 +8,9 @@
     enable = true;
     package = pkgs.opencode;
     settings = {
-      model = "openrouter/z-ai/glm-5.3-flash";
+      model = "openrouter/poolside/laguna-s-2.1:free";
       small_model = "openrouter/poolside/laguna-xs-2.1:free";
-      #model = "openrouter/poolside/laguna-s-2.1:free";
-      #small_model = "openrouter/poolside/laguna-xs-2.1:free";
+      # model = "openrouter/z-ai/glm-5.3-flash";
       # model = "openrouter/nvidia/nemotron-3-super-120b-a12b:free";
       # small_model = "openrouter/nvidia/nemotron-3-nano-30b-a3b:free";
     };
